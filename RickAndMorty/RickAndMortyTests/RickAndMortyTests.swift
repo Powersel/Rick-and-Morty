8e4ad0b4b64341c6080a028fdf723a722654fb6b
Sergiy <sergiy.shevchuk@icloud.com>
2024-11-25 20:06:20 -0500

    Init commit

---
+++ RickAndMorty/RickAndMortyTests/RickAndMortyTests.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyTests.swift
+//  RickAndMortyTests
+//
+//  Created by Sergiy on 11/25/24.
+//
+
+import Testing
+@testable import RickAndMorty
+
+struct RickAndMortyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
